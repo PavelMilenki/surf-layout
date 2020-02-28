a53ca6e134a173a59fb3297dd317fe407dc92c17
Pavel Milenki <54291798+PavelMilenki@users.noreply.github.com>
2020-02-28 16:26:09 +0300

    added finished header

---
+++ src/components/header/Aside.js
@@ -0,0 +1,39 @@
+import React from "react";
+import './header.scss'
+import logo from '../../images/logo.svg'
+import navIcon1 from '../../images/nav-ico-1.svg'
+import navIcon2 from '../../images/nav-ico-2.svg'
+import navIcon3 from '../../images/nav-ico-3.svg'
+import navIcon4 from '../../images/nav-ico-4.svg'
+import search from '../../images/search.svg'
+
+
+export const Aside = () => {
+    return (
+        <div className={'header__aside'}>
+            <div className={'logo'}>
+                <a href="#">
+                    <img src={logo} alt='logo'/>
+                </a>
+            </div>
+            <nav className={'menu'}>
+                <ul className={'menu__list'}>
+                    <li><a href="#"><img src={navIcon1} alt='icon'/><span>Surf</span></a></li>
+                    <li><a href="#"><img src={navIcon2} alt='icon'/><span>Travel</span></a></li>
+                    <li><a href="#"><img src={navIcon3} alt='icon'/><span>Sleep</span></a></li>
+                    <li><a href="#"><img src={navIcon4} alt='icon'/><span>Shop</span></a></li>
+                </ul>
+            </nav>
+            <div className={'header__search'}>
+                <img src={search} alt=""/>
+            </div>
+            <div className={'header__date'}>
+                20
+                <span>06 | 2018</span>
+            </div>
+            <div className={'header__location'}>
+                California
+            </div>
+        </div>
+    )
+};
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,11 +2,22 @@ import React from 'react';
 import './header.scss'
 import {SliderItems} from "./SliderItems";
 import {Dots} from "./Dots";
-
+import {Aside} from "./Aside";
+import {Map} from "./Map";
+import arrowsDown from '../../images/arrows-down.svg'
+import 'animate.css/animate.css'
 
 export function Header() {
     return (
         <header className={'header'}>
+            <div className={'header__title'}>
+                go surf
+            </div>
+            <div className={'header__arrows'}>
+                <a href="#" className={'animated fadeInDown infinite slow'}><img src={arrowsDown} alt='arrow'/></a>
+            </div>
+            <Map/>
+            <Aside/>
             <div className={'header__slider'}>
                 <SliderItems/>
                 <Dots/>
